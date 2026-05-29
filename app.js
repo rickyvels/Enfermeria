ba98ae4f00c64e83814b7b510d67394b44ca20e4
Juan Pablo Cateriano Justo <juanpablocateriano@gmail.com>
2026-05-28 22:47:52 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,48 @@
 console.log("NurseCore iniciado");
+
+function seleccionarUsuario(tipo){
+
+    localStorage.setItem("tipoUsuario", tipo);
+
+    window.location.href = "registro.html";
+}
+
+function irDashboard(){
+
+    const tipo = localStorage.getItem("tipoUsuario");
+
+    if(tipo === "profesional"){
+
+        window.location.href = "dashboard-profesional.html";
+
+    }else{
+
+        window.location.href = "dashboard-estudiante.html";
+
+    }
+}
+
+function mostrarUsuario(){
+
+    const tipo = localStorage.getItem("tipoUsuario");
+
+    const elemento = document.getElementById("usuarioTexto");
+
+    if(!elemento) return;
+
+    if(tipo === "profesional"){
+
+        elemento.innerHTML = "👩‍⚕️ Profesional";
+
+    }else{
+
+        elemento.innerHTML = "🎓 Estudiante";
+
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    mostrarUsuario();
+
+});
